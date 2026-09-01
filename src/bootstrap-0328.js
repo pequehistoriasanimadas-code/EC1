13c376c8e69c8bccea38eb8d2655b8b5ed99f302
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-08-31 23:31:34 -0500

    Install read-only canned cycle status fix in 0.3.28

--- src/bootstrap-0328.js
+++ src/bootstrap-0328.js
@@ -4,3 +4,4 @@ const {PronunciationNormalizer}=require('./services/pronunciation');
 const baseNormalize0326=PronunciationNormalizer.prototype.normalize;
 require('./services/version0327Policy').installVersion0327Policy();
 require('./services/version0328Policy').installVersion0328Policy({baseNormalize0326});
+require('./services/cannedCycle0328').installCycleStatusFix();
