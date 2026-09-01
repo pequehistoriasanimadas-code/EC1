5be1bc1497c21cdcf8f865fda6e827114df8ce6f
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-01 03:06:46 -0500

    Install strict profile switch safety

--- src/bootstrap-0329.js
+++ src/bootstrap-0329.js
@@ -6,4 +6,5 @@ if(!locked){app.quit();}else{
   require('./bootstrap-0328');
   require('./services/profileCompatibility0329').installProfileCompatibility0329();
   require('./services/profilePolicy0329').installVersion0329Profiles();
+  require('./services/profileSwitchSafety0329').installProfileSwitchSafety0329();
 }
