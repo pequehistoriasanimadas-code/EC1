917294e831778e3cb1542319d845182f699e606c
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-01 02:52:45 -0500

    Install 0.3.29 compatibility patches

--- src/bootstrap-0329.js
+++ src/bootstrap-0329.js
@@ -4,5 +4,6 @@ const locked=app.requestSingleInstanceLock();
 if(!locked){app.quit();}else{
   app.on('second-instance',()=>{try{const w=BrowserWindow.getAllWindows().find(x=>!x.isDestroyed()&&!/OUTPUT/i.test(x.getTitle?.()||''));if(w){if(w.isMinimized())w.restore();w.show();w.focus();}}catch{}});
   require('./bootstrap-0328');
+  require('./services/profileCompatibility0329').installProfileCompatibility0329();
   require('./services/profilePolicy0329').installVersion0329Profiles();
 }
