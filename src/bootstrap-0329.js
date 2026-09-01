2add412263591b706b40a24a6a9d4b68404c5972
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-01 12:33:05 -0500

    Wire final 0.3.29 stability layer

--- src/bootstrap-0329.js
+++ src/bootstrap-0329.js
@@ -3,6 +3,8 @@ const {app,BrowserWindow}=require('electron');
 const locked=app.requestSingleInstanceLock();
 if(!locked){app.quit();}else{
   app.on('second-instance',()=>{try{const w=BrowserWindow.getAllWindows().find(x=>!x.isDestroyed()&&!/OUTPUT/i.test(x.getTitle?.()||''));if(w){if(w.isMinimized())w.restore();w.show();w.focus();}}catch{}});
+  const stability=require('./services/profileStability0329');
+  stability.installLegacyWatcherSuppression();
   require('./bootstrap-0328');
   require('./services/profileCompatibility0329').installProfileCompatibility0329();
   require('./services/profilePerformance0329').installProfilePerformance0329();
@@ -13,4 +15,5 @@ if(!locked){app.quit();}else{
   require('./services/profileFinalSafety0329').installProfileFinalSafety0329();
   require('./services/profileSettingsValidation0329').installProfileSettingsValidation0329();
   require('./services/profileHealthFinal0329').installProfileHealthFinal0329();
+  stability.installProfileStability0329();
 }
