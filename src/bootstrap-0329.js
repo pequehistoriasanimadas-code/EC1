1cede644df54ab4c65fd77ff5886f4099658c2c4
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-01 11:34:27 -0500

    Order 0.3.29 performance and final audit fixes

--- src/bootstrap-0329.js
+++ src/bootstrap-0329.js
@@ -5,7 +5,9 @@ if(!locked){app.quit();}else{
   app.on('second-instance',()=>{try{const w=BrowserWindow.getAllWindows().find(x=>!x.isDestroyed()&&!/OUTPUT/i.test(x.getTitle?.()||''));if(w){if(w.isMinimized())w.restore();w.show();w.focus();}}catch{}});
   require('./bootstrap-0328');
   require('./services/profileCompatibility0329').installProfileCompatibility0329();
+  require('./services/profilePerformance0329').installProfilePerformance0329();
   require('./services/profilePolicy0329').installVersion0329Profiles();
   require('./services/profileSwitchSafety0329').installProfileSwitchSafety0329();
   require('./services/profileAuditHardening0329').installProfileAuditHardening0329();
+  require('./services/profileAuditFixes0329').installProfileAuditFixes0329();
 }
