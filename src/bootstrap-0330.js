e9eeb679d64878f72bac56e0a0733b02ff2d88a7
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-02 01:22:49 -0500

    fix: install final 0.3.30 queue projection last

--- src/bootstrap-0330.js
+++ src/bootstrap-0330.js
@@ -2,3 +2,4 @@
 require('./services/editorial0330').installEditorial0330();
 require('./bootstrap-0329');
 require('./services/release0330').installRelease0330();
+require('./services/release0330Final').installRelease0330Final();
