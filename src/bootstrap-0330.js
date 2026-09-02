8a96ed7eebc44c076b49b023c691b0acffa8bf3f
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-02 01:27:17 -0500

    fix: install final profile switch coordinator last

--- src/bootstrap-0330.js
+++ src/bootstrap-0330.js
@@ -3,3 +3,4 @@ require('./services/editorial0330').installEditorial0330();
 require('./bootstrap-0329');
 require('./services/release0330').installRelease0330();
 require('./services/release0330Final').installRelease0330Final();
+require('./services/release0330SwitchFinal').installRelease0330SwitchFinal();
