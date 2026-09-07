82db0bd2baf304e485debcd329f8526d4ea74a04
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-07 13:46:21 -0500

    Keep secondary GEC instances from initializing services

--- src/bootstrap-v2lab.js
+++ src/bootstrap-v2lab.js
@@ -10,7 +10,7 @@ process.env.GEC_V2_TTS_LAB='1';
 const gotLock=app.requestSingleInstanceLock();
 if(!gotLock){app.quit();}else{
   app.on('second-instance',()=>{try{const wins=BrowserWindow.getAllWindows().filter(w=>!w.isDestroyed());const target=wins.find(w=>String(w.getTitle?.()||'').includes('EC Automatic News'))||wins[0];if(target){if(target.isMinimized())target.restore();target.show();target.focus();}}catch{}});
+  require('./bootstrap-0332');
+  require('./services/releaseV2Lab').installReleaseV2Lab();
+  require('./services/releaseV2Optimization').installV2Optimization();
 }
-require('./bootstrap-0332');
-require('./services/releaseV2Lab').installReleaseV2Lab();
-require('./services/releaseV2Optimization').installV2Optimization();
