3029c3facde3d6ec9b3676476df55e5329fdf056
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-10 15:47:42 -0500

    lab.28: install stabilization layer last

--- src/bootstrap-v2lab.js
+++ src/bootstrap-v2lab.js
@@ -29,4 +29,5 @@ if(!gotLock){app.quit();}else{
   require('./services/releaseV2ProductionFidelity').installV2ProductionFidelity();
   require('./services/releaseV2NetworkPermissions').installReleaseV2NetworkPermissions();
   require('./services/releaseV2YoutubePromo').installReleaseV2YoutubePromo();
+  require('./services/releaseV2Stabilization').installReleaseV2Stabilization();
 }
