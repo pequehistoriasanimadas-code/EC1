82a995aa27627bf27ff1df84996163047894b388
pequehistoriasanimadas-code <pequehistoriasanimadas@gmail.com>
2026-09-13 03:27:43 -0500

    feat: enable count-based content recovery policy

--- src/bootstrap-v2lab.js
+++ src/bootstrap-v2lab.js
@@ -32,6 +32,7 @@ if(!gotLock){app.quit();}else{
   require('./services/releaseV2NetworkPermissions').installReleaseV2NetworkPermissions();
   require('./services/releaseV2YoutubePromo').installReleaseV2YoutubePromo();
   require('./services/releaseV2Stabilization').installReleaseV2Stabilization();
+  require('./services/releaseV2CannedRecoveryLab29').installReleaseV2CannedRecoveryLab29();
   require('./services/releaseV2Lab29').installReleaseV2Lab29();
   require('./services/releaseV2EmissionDesign').installReleaseV2EmissionDesign();
   require('./services/releaseV2UxRepairLab29').installReleaseV2UxRepairLab29();
